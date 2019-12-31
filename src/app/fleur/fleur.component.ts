1490f5ab61dbb47fc476bce18285b11ca9cfe0d2
Lucas <i.lucas.guillemette@gmail.com>
2019-12-31 00:07:26 -0500

    navbar, item, style, responsive

--- src/app/fleur/fleur.component.ts
+++ src/app/fleur/fleur.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-fleur',
   templateUrl: './fleur.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FleurComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = () => this.shrinkNav();
+  }
+
+  shrinkNav(){
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+      document.querySelector('.header').classList.add('small')
+    } else {
+      document.querySelector('.header').classList.remove('small')
+    }
+  }
+
+  navPanel(){
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+    document.querySelector('.burger').classList.toggle('toggle')
   }
 
 }
